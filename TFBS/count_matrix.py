5601eec54d1e928d5e8d50b3c4b856bf0000ce25
hermetico <yojmarinv@gmail.com>
2017-09-28 15:32:36 +0200

    minor changes

--- TFBS/count_matrix.py
+++ TFBS/count_matrix.py
@@ -1,14 +1,17 @@
 from math import log
 import numpy as np
+import argparse
 
-def read_sequences_from_file:
-        result = []
+
+def read_sequences_from_file(fname):
+    result = []
     with open(fname) as f:
         for line in f:
             innerList =  [ch for ch in line.strip()]
             if(innerList):
                 result.append(innerList)
     return result
+
 
 def different_letters( lines ):
     seen = []
@@ -18,12 +21,14 @@ def different_letters( lines ):
                 seen.append(ch)
     return seen
 
+
 def count_apperances(letter, sequences, position):
     counter = 0
     for sequence in sequences():
         if(sequence[position] == letter):
             counter+=1
     return counter
+
 
 def create_count_matrix(sequences):
     letters = sequences[0].length
@@ -34,3 +39,13 @@ def create_count_matrix(sequences):
             count_matrix[i][j] = count_apperances(different_letters(j), sequences, i)
     return count_matrix
 
+
+
+if __name__ == "__main__":
+    # Giving the name of the file
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file",
+                        required=True,
+                        type=str,
+                        default=None,
+                        help="File containing sequences")
