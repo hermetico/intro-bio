013ecd56d7b0f159bd372319005773774f7b072d
Juan Marín Vega <yojmarinv@gmail.com>
2017-09-30 20:22:43 +0200

    count_matrix also generates seq-logo

--- TFBS/count_matrix.py
+++ TFBS/count_matrix.py
@@ -1,7 +1,8 @@
 import numpy as np
 import argparse
-
-FREQs = {'a': .2, 't': 0.2, 'c': .3, 'g': .3}
+import seq_logo_plot
+import math
+FREQs = {'a': .2, 't': .2, 'c': .3, 'g': .3}
 
 
 def read_sequences_from_file(fname):
@@ -20,7 +21,7 @@ def create_pwm_count_matrix(sequences, aas):
     for i, ch in enumerate(aas):
         count_matrix[i,:] = (sequences == ch).sum(axis=0)
 
-    return np.matrix(count_matrix)
+    return count_matrix
 
 
 def create_pwm_freq_matrix( pwm_count, pseudo_counts=None, back_freq=None):
@@ -37,15 +38,37 @@ def create_pwm_freq_matrix( pwm_count, pseudo_counts=None, back_freq=None):
     return pwm_count / pwm_count.sum(axis=0)
 
 
+
+def create_relative_entropy(probs, back_probs):
+    """Computes the relative entropy"""
+    if np.any(probs == 0):
+        RH = np.zeros(probs.shape[1])
+        for j in range(probs.shape[1]):
+            accum = 0.0
+            for i in range(back_probs.shape[0]):
+                if probs[i][j] != 0.0 and back_probs[i] != 0.0:  # avoiding zeros
+                    # the sum in a sequence column of the probability of each letter times the logarithm in base 2
+                    # of the probability of a letter divided by its background freq
+                    accum += probs[i][j] * np.log2(probs[i][j] / back_probs[i])
+            RH[j] = accum
+        return RH
+
+    return np.sum( probs *  np.log2( probs / back_probs),
+        axis=0).ravel()
+
+
+
 def print_matrix( m, precision=2):
     """Prints the matrix with the selected precision"""
     for i in range(m.shape[0]):
-        print ", ".join(["%.*f" %(precision, num) for num in m[i].getA1()])
+        print ", ".join(["%.*f" %(precision, num) for num in m[i]])
     print("")
 
 
 
 if __name__ == "__main__":
+
+    ploter = seq_logo_plot.Logo_Ploter()
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--file",
@@ -54,12 +77,13 @@ if __name__ == "__main__":
                         default=None,
                         help="File containing sequences")
 
-    sequences = read_sequences_from_file("input.txt")
+    args = parser.parse_args()
+
+    sequences = read_sequences_from_file(args.file)
 
     AAs = np.unique(sequences)
     PI = np.array([FREQs[a] for a in AAs]) # one array
     PI = PI[:, np.newaxis] #2D array with one column
-
     PWM_count = create_pwm_count_matrix(sequences, AAs)
 
     print("PI:")
@@ -69,21 +93,36 @@ if __name__ == "__main__":
     print("\nPWM count")
     print(PWM_count)
     print("\nPWM freq")
-    print("With pseudocounts 0")
-    pwm = create_pwm_freq_matrix(PWM_count)
-    assert(np.sum(pwm.sum(axis=0)) == pwm.shape[1]) # checks everything is normalized
-    print_matrix(pwm)
-    print("With pseudocounts 1")
-    pwm = create_pwm_freq_matrix(PWM_count, pseudo_counts=1, back_freq=PI)
-    assert (np.sum(pwm.sum(axis=0)) == pwm.shape[1])
-    print_matrix(pwm)
-    print("With pseudocounts 2")
-    pwm = create_pwm_freq_matrix(PWM_count, pseudo_counts=2, back_freq=PI)
-    assert (np.sum(pwm.sum(axis=0)) == pwm.shape[1])
-    print_matrix(pwm)
 
-    print("With pseudocounts 4")
-    pwm = create_pwm_freq_matrix(PWM_count, pseudo_counts=4, back_freq=PI)
-    assert (np.sum(pwm.sum(axis=0)) == pwm.shape[1])
-    print_matrix(pwm)
+
+    for pseudocount in [0, 1, 3, 5]:
+        print("With pseudocounts %i" %(pseudocount))
+        if pseudocount == 0:
+            pwm = create_pwm_freq_matrix(PWM_count)
+        else:
+            pwm = create_pwm_freq_matrix(PWM_count, pseudo_counts=pseudocount, back_freq=PI)
+
+        assert(np.sum(pwm.sum(axis=0)) == pwm.shape[1]) # checks everything is normalized
+        print_matrix(pwm)
+        """
+        RH = create_relative_entropy(pwm, PI)
+        C = pwm * RH  # contributions
+
+        scores = ploter.create_scores(C, AAs)
+
+        ploter.draw_logo(C, AAs, filename="seq-logo%i.png" %(pseudocount))
+        """
+
+
+    probs = create_pwm_freq_matrix(PWM_count)
+    RH = create_relative_entropy(probs, PI)
+    C = probs * RH  # contributions
+
+    scores = ploter.create_scores(C, AAs)
+
+    ploter.draw_logo(C, AAs, filename="seq-logo.png")
+
+
+
+
 
--- TFBS/seq_logo_plot.py
+++ TFBS/seq_logo_plot.py
@@ -3,18 +3,19 @@ from matplotlib import transforms
 import matplotlib.patheffects
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
-COLOR_SCHEME = {'A': 'black',
+COLOR_SCHEME = {'A': 'green',
                 'B': 'rosybrown',
-                'C': 'firebrick',
+                'C': 'darkblue',
                 'D': 'tan',
                 'E': 'gold',
                 'F': 'olivedrab',
-                'G': 'chartreuse',
+                'G': 'orange',
                 'H': 'darkgreen',
                 'I': 'lightseagreen',
                 'J': 'deepskyblue',
-                'K': 'blue',
+                'K': 'indigo',
                 'L': 'palevioletred',
                 'M': 'orange',
                 'N': 'salmon',
@@ -23,7 +24,7 @@ COLOR_SCHEME = {'A': 'black',
                 'Q': 'orange',
                 'R': 'yellow',
                 'S': 'olive',
-                'T': 'indigo',
+                'T': 'firebrick',
                 'U': 'dodgerblue',
                 'V': 'navajowhite',
                 'W': 'fuchsia',
@@ -41,50 +42,162 @@ class Scale(matplotlib.patheffects.RendererBase):
         renderer.draw_path(gc, tpath, affine, rgbFace)
 
 
+class Logo_Ploter(object):
 
-def draw_logo(all_scores, size=40, filename="plot.png", ymax=2):
-    if sys.platform == 'win32':
-        font_family = 'Arial'
-    else:
-        font_family = 'sans-serif'
+    def create_scores(self, probs , aas):
+        """Creates the scores for the plot, and sorts them"""
+        scores = []
+        for i in range(probs.shape[1]):
+            position = []
+            for j in range(aas.shape[0]):
+                prob = probs[j][i]
+                if probs[j][i] > 0.0:
+                    position.append((aas[j].upper(),prob))
+            scores.append(sorted(position, key=lambda x:x[1]))
 
-    fig, ax = plt.subplots()
-    fig.set_size_inches(len(all_scores), 5)  ###
-    ax.set_xticks(range(1, len(all_scores) + 1))
-    ax.set_yticks(range(0, ymax))
-    ax.set_xticklabels(range(1, len(all_scores) + 1))
-    ax.set_yticklabels(np.arange(0, ymax))
+        return scores
 
-    trans_offset = transforms.offset_copy(ax.transAxes,
-                                          fig=fig,
-                                          x=0,
-                                          y=0,
-                                          units='points')
-    xshift = 0
-    for index, scores in enumerate(all_scores):
-        for base, score in scores:
-            txt = ax.text(0.0555,
-                          0,
-                          base,
-                          transform=trans_offset,
-                          fontsize=70,
-                          color=COLOR_SCHEME[base],
-                          weight='bold',
-                          ha='center',
-                          family=font_family)
+    def create_scores_norm(self, probs , aas):
+        """Creates the scores for the plot but normalized, and sorts them"""
+        scores = []
+        for i in range(probs.shape[1]):
+            position = []
+            mmax = np.sum(probs[:, i])
+            for j in range(aas.shape[0]):
+                prob = probs[j][i]
+                if probs[j][i] > 0.0:
+                    position.append((aas[j].upper(), prob / mmax))
+            scores.append(sorted(position, key=lambda x:x[1]))
 
-            txt.set_clip_on(False)
-            txt.set_path_effects([Scale(1.0, score)])
-            fig.canvas.draw()
-            window_ext = txt.get_window_extent(txt._renderer)
-            yshift = window_ext.height * (score * .6)  ###
-            trans_offset = transforms.offset_copy(txt._transform, fig=fig,
-                                                  y=yshift,
-                                                  units='points')
-        xshift += window_ext.width * 0.8  ###
-        trans_offset = transforms.offset_copy(ax.transAxes, fig=fig,
-                                              x=xshift,
+        return scores
+
+    def draw_logo_norm(self, probs, aas, size=40, filename=None):
+
+        scores = self.create_scores_norm(probs, aas)
+        num_letters = aas.shape[0]
+        ymax = int(math.ceil(math.log(num_letters, 2)))
+
+        plt.rcParams['axes.labelsize'] = 14
+        plt.rcParams['axes.labelweight'] = 'bold'
+        if sys.platform == 'win32':
+            font_family = 'Arial'
+        else:
+            font_family = 'sans-serif'
+
+        fig, ax = plt.subplots()
+        fig.set_size_inches(len(scores), (ymax + 0.5) * 0.75)  ###
+        ax.set_xticks(range(1, len(scores) + 1))
+        ax.set_yticks(range(0, ymax + 1))
+        ax.set_xticklabels(range(1, len(scores) + 1))
+        ax.set_yticklabels(np.arange(0, ymax + 1))
+        ax.set_ylabel('bits')
+        ax.xaxis.set_tick_params(labelsize=14)
+        ax.yaxis.set_tick_params(labelsize=14)
+
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+        ax.spines['left'].set_visible(True)
+
+        trans_offset = transforms.offset_copy(ax.transAxes,
+                                              fig=fig,
+                                              x=0,
+                                              y=0,
                                               units='points')
-    plt.savefig(filename)
+        xshift = 0
+        for index, position_scores in enumerate(scores):
+            max_contribution = 1. * ymax / len(position_scores)
+            for base, rel_score in position_scores:
+                # normalizes with maximum contribution posible on a given position
+                score = rel_score * max_contribution
+                txt = ax.text(0.09,
+                              0,
+                              base,
+                              transform=trans_offset,
+                              fontsize=70,
+                              color=COLOR_SCHEME[base],
+                              weight='bold',
+                              ha='center',
+                              family=font_family)
 
-    # plt.show()
+                txt.set_clip_on(False)
+                txt.set_path_effects([Scale(1.0, score)])
+                fig.canvas.draw()
+                window_ext = txt.get_window_extent(txt._renderer)
+                yshift = window_ext.height * (score * 0.6)  ###
+                trans_offset = transforms.offset_copy(txt._transform, fig=fig,
+                                                      y=yshift,
+                                                      units='points')
+            xshift += window_ext.width * 0.8  ###
+            trans_offset = transforms.offset_copy(ax.transAxes, fig=fig,
+                                                  x=xshift,
+                                                  units='points')
+        if filename is not None:
+            plt.savefig(filename)
+        else:
+            plt.show()
+
+    def draw_logo(self, probs, aas, size=40, filename=None):
+
+        scores = self.create_scores(probs, aas)
+        num_letters = aas.shape[0]
+        ymax = int(math.ceil(math.log(num_letters, 2)))
+
+        plt.rcParams['axes.labelsize'] = 14
+        plt.rcParams['axes.labelweight'] = 'bold'
+        if sys.platform == 'win32':
+            font_family = 'Arial'
+        else:
+            font_family = 'sans-serif'
+
+        fig, ax = plt.subplots()
+        fig.set_size_inches(len(scores), (ymax + 0.5) * 0.75)  ###
+        ax.set_xticks(range(1, len(scores) + 1))
+        ax.set_yticks(range(0, ymax + 1))
+        ax.set_xticklabels(range(1, len(scores) + 1))
+        ax.set_yticklabels(np.arange(0, ymax + 1))
+        ax.set_ylabel('bits')
+        ax.xaxis.set_tick_params(labelsize=14)
+        ax.yaxis.set_tick_params(labelsize=14)
+
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+        ax.spines['left'].set_visible(True)
+
+        trans_offset = transforms.offset_copy(ax.transAxes,
+                                              fig=fig,
+                                              x=0,
+                                              y=0,
+                                              units='points')
+        xshift = 0
+        for index, position_scores in enumerate(scores):
+            for base, rel_score in position_scores:
+
+                score = rel_score
+                txt = ax.text(0.09,
+                              0,
+                              base,
+                              transform=trans_offset,
+                              fontsize=70,
+                              color=COLOR_SCHEME[base],
+                              weight='bold',
+                              ha='center',
+                              family=font_family)
+
+                txt.set_clip_on(False)
+                txt.set_path_effects([Scale(1.0, score)])
+                fig.canvas.draw()
+                window_ext = txt.get_window_extent(txt._renderer)
+                yshift = window_ext.height * (score * 0.6)  ###
+                trans_offset = transforms.offset_copy(txt._transform, fig=fig,
+                                                      y=yshift,
+                                                      units='points')
+            xshift += window_ext.width * 0.8  ###
+            trans_offset = transforms.offset_copy(ax.transAxes, fig=fig,
+                                                  x=xshift,
+                                                  units='points')
+        if filename is not None:
+            plt.savefig(filename)
+        else:
+            plt.show()
