e71da12024a58ed8cb6c2214c5af651670750a01
hermetico <yojmarinv@gmail.com>
2017-11-04 14:21:48 +0100

    f_score implementation

--- aco/aco.py
+++ aco/aco.py
@@ -36,6 +36,7 @@ def compute_distances( nodes , positions):
     for a in nodes:
         for b in nodes:
             if a != b:
+                # only stores a,b or b,a
                 if not (a, b) in distances and not (b, a) in distances: # use a=distances((a,b)) or distances((b,a))
                     pair = (a, b)
                     distances[pair] = euclidean(positions[a], positions[b])
@@ -44,35 +45,45 @@ def compute_distances( nodes , positions):
 
 class ACO(object):
     def __init__(self, nodes, positions):
+        """Initialize the ACO"""
         self.nodes = np.array(nodes, dtype=int)
         self.positions = np.array(positions)
         self.distances = compute_distances(self.nodes, self.positions)
         self.pairs = { key: None for key in self.distances.keys() }
 
     def setup(self, alpha=None, beta=None, evaporation=None, num_ants=None, visualization=False):
+        """Setup parameters and initializations"""
         self.alpha = alpha or 1.0
         self.beta = beta or 1.0
         self.evaporation = evaporation or 1.0
         self.num_ants = num_ants or 10
-        # reset pheromones
+        # set all pheromones to 1.0
         self.pheromones = {key: 1.0 for key in self.distances.keys() }
 
         # this function is now optimized to be ran over vectors
+        # vectorize __compute_probs__
+        # Then we can use v_compute_probs with a numpy array
         self.v_compute_probs = np.vectorize(ACO.__compute_probs__)
+
+        # initializes the visualization class
         self.visualization = visualization
         if self.visualization:
             self.vis = VTSP(self.positions, invert=True)
 
     def __correct_pair__(self, a, b):
+        """Returns the correct pair associated to those nodes"""
         if (a, b) in self.pairs:
             return (a, b)
         return (b, a)
 
     @staticmethod
+    ## Static method which is going to be optimized to run over a vector
     def __compute_probs__(x ,y, pheromones, distances, alpha, beta,  pick_pair):
+        """Computes tau^alpha * (1/eta)^beta"""
         return math.pow(pheromones[pick_pair(x,y)], alpha) * math.pow(1.0 / distances[pick_pair(x,y)], beta)
 
     def run(self):
+        """Runs everything"""
         if self.visualization:
            self.vis.init()
 
@@ -86,7 +97,8 @@ class ACO(object):
         while convergence_iterations:
 
             ## construct solutions
-            routes, route_distances = self.__step__(self.num_ants, self.nodes, self.distances, self.pheromones, self.alpha, self.beta)
+            routes, route_distances = self.__step__(self.num_ants, self.nodes, self.distances,
+                                                    self.pheromones, self.alpha, self.beta)
 
             ## evaluate solutions
             i_min_distance = route_distances.min()
@@ -104,6 +116,8 @@ class ACO(object):
             ## update pheromones
             self.__update_pheromones__(routes, route_distances, self.pheromones, self.evaporation)
 
+            ## shows visualization each iterations
+            ## otherwise the window becomes buggy
             if self.visualization:
                 self.vis.update_route(best_route)
 
@@ -111,6 +125,7 @@ class ACO(object):
             iter += 1
             prev_best_distance = best_distance
 
+        # after everything, this fixes the window
         if self.visualization:
             self.vis.freeze( best_distance )
 
@@ -120,9 +135,9 @@ class ACO(object):
         """Perform a complete step of the algorithm"""
         route_distances = np.zeros(ants)
         routes = np.zeros((ants, nodes.shape[0] + 1), dtype=int)
+        # one computation per ant
         for i in range(self.num_ants):
             routes[i,:], route_distances[i] = self.__ant_solve__(nodes, distances, pheromones, alpha, beta)
-
         return routes, route_distances
 
     def __ant_solve__(self, nodes, distances, pheromones, alpha, beta):
@@ -140,18 +155,21 @@ class ACO(object):
         remaining -= 1
 
         while remaining:
+            # collects all remaining nodes
             rem_neighbors = nodes[rem_nodes]
+
             if len(rem_neighbors) > 1:
                 next_node = self.__next_node__(current, rem_neighbors, distances, pheromones, alpha, beta )
             else:
                 next_node = rem_neighbors[0]
+
             route.append(next_node)
             rem_nodes[next_node] = 0
             route_distance += distances[self.__correct_pair__(current, next_node)]
             remaining -= 1
             current = next_node
 
-        # appends the initial node to complete the route
+        # appends the initial node to complete the circular route and updates the distance
         route.append(init)
         route_distance += distances[self.__correct_pair__(current,  init)]
 
@@ -159,11 +177,12 @@ class ACO(object):
 
     def __next_node__(self, _from, rem_neighbors, distances, pheromones, alpha, beta):
         """returns the next node based on the probabilities"""
+        # applies the vectorized function to the rem_neighbors array
         rem_probs = self.v_compute_probs(rem_neighbors, _from,  pheromones, distances, alpha, beta,  self.__correct_pair__)
-        sum_probs = rem_probs.sum()
+        sum_probs = rem_probs.sum() # sums everything to normalize it afterwards
 
         if sum_probs != 0.0:
-            # picks one node based on their probabilities
+            # picks one neighbor based on the probabilities normalized
             return rem_neighbors[ np.where(np.random.multinomial(1, rem_probs / sum_probs))][0]
 
         #if the sum of the probs is 0, just pick the argument with the maximum probability
--- clustering/f_score.py
+++ clustering/f_score.py
@@ -2,15 +2,62 @@ import numpy as np
 import argparse
 import math
 
+def read_input( filename ):
+    data = []
+    cluster_data = []
+    current_cluster = 0
+    with open(filename) as f:
+        for line in f:
+            parts = line.rstrip().split('\t')
+            if len(parts) == 2:
+                if int(parts[1]) != current_cluster:
+                    data.append(cluster_data)
+                    current_cluster = int(parts[1])
+                    cluster_data = []
+                cluster_data.append(parts[0])
 
+        if len(cluster_data) > 0:
+            data.append(cluster_data)
+    return data
+
+
+def mapping_clusters( cla, clb):
+    mapping = {}
+    for i, data_a in enumerate(cla):
+
+        commons = [0] * len(clb)
+        for j, data_b in enumerate(clb):
+            commons[j] = sum([ 1 for a in data_a if a in data_b])
+
+        mapping[i] = np.array(commons).argmax()
+    return mapping
+
+def compute_f_score(cla, clb, mappings):
+
+    for i, cl in enumerate(cla):
+        mapp = clb[mappings[i]]
+        TP = sum([1 for a in cl if a in mapp]) * 1.
+        FP = sum([1 for a in cl if a not in mapp]) * 1.
+        FN = sum([1 for a in mapp if a not in cl]) * 1.
+        #TN = sum([1 for a in mapp if a in cl]) * 1.
+
+    precission = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    return  2 * precission * recall / (precission + recall)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--file", required=True, type=str, default=None, help="File containing results")
-    parser.add_argument("-g", "--gold", required=True, type=str, default=1.0, help="File containing gold standards")
+    parser.add_argument("-g", "--gold", required=True, type=str, default=None, help="File containing gold standards")
 
 
     args = parser.parse_args()
 
-    results = read_results(args.file)
-    results = read_gold(args.file)
+    results = read_input(args.file)
+    gold = read_input(args.gold)
+    if len(results) != len(gold):
+        print "There aren't the same number of clusters"
+        exit(1)
+    mappings = mapping_clusters(results, gold)
+    f_score = compute_f_score(results, gold, mappings)
+    print f_score
